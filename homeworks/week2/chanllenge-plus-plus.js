44f59b9ea801126ed92e197589aed98feb732672
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-06-22 21:31:04 +0800

    week2 completed

---
+++ homeworks/week2/chanllenge-plus-plus.js
@@ -0,0 +1,17 @@
+function multiply(a, b) {
+  let resultArr = new Array(a.length + b.length).fill(0);
+  for(let i = a.length; i--;) {
+    let carry = 0;
+    for(let k = b.length; k--;) {
+      carry = Math.floor( (resultArr[1+i+k] + (Number(a[i]) * Number(b[k])) ) / 10);
+      resultArr[1+i+k] = (resultArr[1+i+k] + (Number(a[i]) * Number(b[k]))) % 10;
+      resultArr[i+k] = resultArr[i+k] + carry;
+    }
+  }
+  resultArr = resultArr.join("")
+  resultArr = resultArr.replace(/^0/g, "")
+  return resultArr
+
+}
+
+console.log(multiply("12345","12345"))
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,25 @@
 function join(arr, concatStr) {
+  let joinedStr = "";
+  for(let i = 0; i < arr.length; i++) {
+    joinedStr += String(arr[i]);
+    joinedStr += concatStr;
+  }
+
+  return joinedStr
   
 }
 
 function repeat(str, times) {
-  
+  let repeatedStr = '';
+  for(let i = 0; i < times; i++) {
+    repeatedStr += str;
+  }
+  return repeatedStr
 }
 
-console.log(join(['a'], '!'));
+console.log(join([1,2,3], ''));
+console.log(join(["a", "b", "c"], "!"));
+console.log(join(["a", 1, "b", 2, "c", 3], ','));
+
 console.log(repeat('a', 5));
+console.log(repeat('yoyo', 2));
