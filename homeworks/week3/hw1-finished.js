e15de369ee7ab37452181aa967c1e3bcdfcbd032
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-07-01 18:16:36 +0800

    home work amended

--- homeworks/week3/hw1-finished.js
+++ homeworks/week3/hw1-finished.js
@@ -1,10 +1,33 @@
+const readline = require('readline');
 
-function printStar(n) {
-  let star = "";
-  for(let i = 0; i < n; i++) {
-    star +="*";
-    console.log(star);
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+const lines = [];
+
+// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
+rl.on('line', (line) => {
+  lines.push(line);
+});
+
+// 輸入結束，開始針對 lines 做處理
+rl.on('close', () => {
+  // eslint-disable-next-line no-use-before-define
+  solve(lines);
+});
+
+// 上面都不用管，只需要完成這個 function 就好，可以透過 lines[i] 拿取內容
+function solve(data) {
+  const n = Number(data[0]);
+
+  function printStar(num) {
+    let star = '';
+    for (let i = 0; i < num; i += 1) {
+      star += '*';
+      console.log(star);
+    }
   }
+  printStar(n);
 }
-
-printStar(10)
+solve(lines);
--- homeworks/week3/hw5-finished.js
+++ homeworks/week3/hw5-finished.js
@@ -1,44 +1,61 @@
-let lines = ["3","1 2 1", "1 2 -1", "2 2 1"];
+const readline = require('readline');
 
-solve(lines)
+const rl = readline.createInterface({
+  input: process.stdin,
+});
 
-function solve(lines) {
+const lines = [];
 
-  lines = lines.map(item => {
-      item = item.split(' ');
-      item = item.map(s => {
-           s = BigInt(s);
-          return s;
-      })
-      return item;
-   })
+// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
+rl.on('line', (line) => {
+  lines.push(line);
+});
 
-   let m = lines.shift();
+// 輸入結束，開始針對 lines 做處理
+rl.on('close', () => {
+  // eslint-disable-next-line no-use-before-define
+  solve(lines);
+});
 
-   if(m >= 1 && m <= 50) {
-    for(let i = 0; i < m; i++) {
-        let [a, b, k] = lines[i];
-        if(k === 1n) {
-            if(a === b) {
-                console.log('DRAW');
-              } else {
-                  let win = a > b ? "A" : "B";
-                  console.log(win);
-              }
-            
-              
-          } else if(k === -1n) {
-              if(a === b) {
-                  console.log('DRAW');
-              } else {
-                  let win = a < b ? "A" : "B";
-                  console.log(win);
-              }
-          } else {
-              continue
+// 上面都不用管，只需要完成這個 function 就好，可以透過 lines[i] 拿取內容
+function solve(data) {
+  const lines1 = data.map((item) => {
+    const item1 = item.split(' ');
+    const item2 = item1.map((s) => {
+      // eslint-disable-next-line no-undef
+      const s1 = BigInt(s);
+      return s1;
+    });
+    return item2;
+  });
+
+
+  const m = lines1.shift();
+  if (m >= 1 && m <= 50) {
+    for (let i = 0; i < m; i += 1) {
+      const [a, b, k] = lines1[i];
+      // eslint-disable-next-line no-undef
+      const n1 = BigInt(1);
+      // eslint-disable-next-line no-undef
+      const n2 = BigInt(-1);
+      if (k === n1) {
+        if (a === b) {
+          console.log('DRAW');
+        } else {
+          const win = a > b ? 'A' : 'B';
+          console.log(win);
         }
+      } else if (k === n2) {
+        if (a === b) {
+          console.log('DRAW');
+        } else {
+          const win = a < b ? 'A' : 'B';
+          console.log(win);
+        }
+      } else {
+        // eslint-disable-next-line no-continue
+        continue;
+      }
     }
-       
-   }
-
+  }
 }
