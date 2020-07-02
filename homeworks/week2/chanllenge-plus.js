d323cd69cfcd4e5ccb2a294191f14618af7755d4
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-07-02 21:05:55 +0800

    updating

--- homeworks/week2/chanllenge-plus.js
+++ homeworks/week2/chanllenge-plus.js
@@ -1,11 +1,13 @@
-function add(a, b){
-    let carry = 0;
-    while(b) {
-        carry = a & b;
-        a = a ^ b;
-        b = carry << 1
-    }
-    return a;
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-bitwise */
+function add(a, b) {
+  let carry = 0;
+  while (b) {
+    carry = a & b;
+    a ^= b;
+    b = carry << 1;
+  }
+  return a;
 }
 
-console.log(add(10,8));
+console.log(add(10, 8));
--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,8 +1,8 @@
 function printStars(n) {
-  if(n >= 1 && n <= 30) {
-    let star = "*";
-    for(let i = 0; i < n; i++) {
-      console.log(star)
+  if (n >= 1 && n <= 30) {
+    const star = '*';
+    for (let i = 0; i < n; i++) {
+      console.log(star);
     }
   }
 }
